c99741ed42e7b5e8cade522ed6c4d6f8ecf3f668
Tea Flores-Kestner <dfkestner@gmail.com>
2020-11-15 18:27:31 -0500

    added lite-server, morgan, and compression dependencies and updates server.js

--- server.js
+++ server.js
@@ -1,10 +1,15 @@
 const express = require("express");
+const logger = require("morgan");
 const mongoose = require("mongoose");
+const compression = require("compression");
 
 const PORT = process.env.PORT || 3002
 
 const app = express();
 
+app.use(logger("dev"));
+
+app.use(compression());
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
@@ -12,12 +17,13 @@ app.use(express.static("public"));
 
 mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/budget", {
   useNewUrlParser: true,
-  useFindAndModify: false
+  useFindAndModify: false,
+  useUnifiedTopology: true
 });
 
 // routes
 app.use(require("./routes/api.js"));
 
 app.listen(PORT, () => {
-  console.log(`App running on port ${PORT}!`);
+  console.log(`App running http://localhost:${PORT} !`);
 });
